b8300e28aaf6351e13d0d368329ab943d82f637a
adam <adam@reddit.com>
2015-06-14 18:44:11 -0700

    update package.js

--- package.js
+++ package.js
@@ -2,21 +2,16 @@ Package.describe({
   name: 'less-neat',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'joasakuras less implementation of the bourbon neat grid as a meteor package.',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'lib/less-neat/README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('less-neat.js');
+  api.addFiles('lib/stylesheets/_less-neat.less', 'client');
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('less-neat');
-  api.addFiles('less-neat-tests.js');
-});
