5f17e608425c76e7f7506bd366994dc77e512127
adam <adam@reddit.com>
2015-06-14 18:44:51 -0700

    remove extra readme

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'less-neat',
+  name: 'curioussavage:less-neat',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'joasakuras less implementation of the bourbon neat grid as a meteor package.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/curioussavage/meteor-less-neat',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'lib/less-neat/README.md'
@@ -12,6 +12,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('lib/stylesheets/_less-neat.less', 'client');
+  api.addFiles('lib/less-neat/stylesheets/less-neat.less', 'client');
 });
 
