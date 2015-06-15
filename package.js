d16373c2d29c33fcbb6bf822ab3c90c616105070
adam <adam@reddit.com>
2015-06-14 19:12:34 -0700

    bump version to 0.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'curioussavage:less-neat',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'joasakuras less implementation of the bourbon neat grid as a meteor package.',
   // URL to the Git repository containing the source code for this package.
