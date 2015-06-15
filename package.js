d7152752a556f7e7093f3d2d9600036aece4fba8
adam <adam@reddit.com>
2015-06-14 19:40:07 -0700

    abandon submodule for now

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'curioussavage:less-neat',
-  version: '0.0.2',
+  version: '0.2.0',
   // Brief, one-line summary of the package.
   summary: 'joasakuras less implementation of the bourbon neat grid as a meteor package.',
   // URL to the Git repository containing the source code for this package.
@@ -12,6 +12,18 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('lib/less-neat/stylesheets/less-neat.less', 'client');
+  api.addFiles('lib/stylesheets/less-neat.less', 'client');
+  api.addFiles('lib/stylesheets/core/_functions.less', 'client');
+  api.addFiles('lib/stylesheets/core/_utils.less', 'client');
+  api.addFiles('lib/stylesheets/core/_variables.less', 'client');
+
+  api.addFiles('lib/stylesheets/grid/_fill-parent.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_omega.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_outer-container.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_pad.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_row.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_shift.less', 'client');
+  api.addFiles('lib/stylesheets/grid/_span-columns.less', 'client');
+
 });
 
